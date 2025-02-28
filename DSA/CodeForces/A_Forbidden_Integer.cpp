eabc9457a69a1eedb0c1ef80f734a536a7c0d621
Anubrata-Mallick <anubrata2003snr@outlook.com>
2025-03-01 01:15:02 +0530

    1-march-2025-sat

--- DSA/CodeForces/A_Forbidden_Integer.cpp
+++ DSA/CodeForces/A_Forbidden_Integer.cpp
@@ -21,23 +21,51 @@ int main(){
     int t; cin >> t;
     while(t--){
 
-    //take Input 
-    int n, k, x; cin >>n>>k>>x;
+        //take Input 
+        int n, k, x; cin >>n>>k>>x;
     
-    // Write Logic 
-        if(x!=1){
-            cout<<"YES" <<endl;
-            cout << n << endl; // print array size
-            loop(i, 0, n) cout << 1 << " "; // all will be 1
-            cout <<endl;
+        // Write Logic
+        if(x != 1){
+            cout << "YES"<<endl;
+            cout<<n<<endl;
+            loop(i, 0, n) cout << 1 << " ";
+            cout<< endl;
         }else{
-            if(k>=3){
-              cout<<"YES" <<endl;
-              // 11 -> five 2s and one 3
-              
+            if(k == 1){
+                cout << "NO" << endl;
+            }else if( k==2 ){
+                if(iseven(n)){
+                    cout<<"YES"<<endl;
+                    cout<<n/2<<endl;
+                    loop(i, 0, n/2) cout << 2 <<" ";
+                    cout<<endl;
+                }else{
+                    cout<<"NO"<<endl;
+                }
+            }else{
+                cout<<"YES"<<endl;
+                if(iseven(n)){
+                    cout<<n/2<<endl;
+                    loop(i, 0, n/2) cout << 2 <<" ";
+                    cout<<endl;
+                }
+                else{
+                    n==3 ? cout<<1<<endl : cout << 1 + ((n-3)/2)<<endl;
+                    cout <<3<<" ";
+                    loop(i, 0, (n-3)/2) cout << 2 <<" ";
+                    cout<<endl;
+                }
             }
-        }
 
+        }
     }
     return 0;
 }
+
+/*
+if x is not 1 (1 is available in series) -> can make any number by adding 1s 
+if x is 1 ( 1 is not available in series )
+    -> (k == 1) -> there is no element so "NO"
+    -> (k == 2) -> only 2 present so "YES" for even "NO" for odd
+    -> (k == 3) -> "YES" because we can make any number using 2,3 / to make even numer just use 2 / to make odd number start with 3 and add 2s
+*/
